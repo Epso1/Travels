1aef95bcfd3bef48d61ab9c9442cc3efecd6675f
Epso1 <cesaralvarorubert@gmail.com>
2023-12-04 18:34:19 +0100

    Colores de toolbar cambiados en MainActivity

--- app/src/main/java/com/example/travels/MainActivity.java
+++ app/src/main/java/com/example/travels/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.travels;
 import static com.example.travels.ImageAdapter.getImageList;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,30 +20,25 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
 
-    public static final List<Integer> imagenes = Arrays.asList(
-            R.drawable.image1,
-            R.drawable.image2,
-            R.drawable.image3,
-            R.drawable.image4,
-            R.drawable.image5,
-            R.drawable.image6,
-            R.drawable.image7,
-            R.drawable.image8,
-            R.drawable.image9,
-            R.drawable.image10
-    );
     private ImageSwitcher imageSwitcher;
-    private ImageAdapter imageAdapter;
+    private ImageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setTitleTextColor(getColor(R.color.white));
+        toolbar.setBackgroundColor(getColor(R.color.light_green));
+
+        setSupportActionBar(toolbar);
+
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        ImageAdapter adapter = new ImageAdapter();
+        adapter = new ImageAdapter();
         recyclerView.setAdapter(adapter);
         imageSwitcher = findViewById(R.id.imageSwitcher);
         imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
